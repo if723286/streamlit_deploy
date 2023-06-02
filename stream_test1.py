cbb3f15ff476a64203de150bd3c58d015701ea6f
Blas Sanchez <if723286@iteso.mx>
2023-06-02 14:20:04 -0600

    Modifique monto maximo

--- stream_test1.py
+++ stream_test1.py
@@ -12,27 +12,27 @@ def asignar_tasa_interes(monto_prestamo, periodos, primera_vez):
             return random.uniform(7.7, 8.3)
     else:
         # Monto de 2,000
-        if monto_prestamo <= 2000 and periodos <= 6:
+        if monto_prestamo <= 200 and periodos <= 6:
             return random.uniform(6.5, 6.7)
-        elif monto_prestamo <= 2000 and periodos <= 12:
+        elif monto_prestamo <= 200 and periodos <= 12:
             return random.uniform(6.8, 7.0)
         # Monto de 4,000
-        elif monto_prestamo <= 4000 and periodos <= 6:
+        elif monto_prestamo <= 400 and periodos <= 6:
             return random.uniform(6.0, 6.2)
-        elif monto_prestamo <= 4000 and periodos <= 12:
+        elif monto_prestamo <= 400 and periodos <= 12:
             return random.uniform(6.3, 6.5)
         # Monto de 6,000
-        elif monto_prestamo <= 6000 and periodos <= 6:
+        elif monto_prestamo <= 600 and periodos <= 6:
             return random.uniform(5.5, 5.7)
-        elif monto_prestamo <= 6000 and periodos <= 12:
+        elif monto_prestamo <= 600 and periodos <= 12:
             return random.uniform(5.8, 6.0)
         # Monto de 8,000
-        elif monto_prestamo <= 8000 and periodos <= 6:
+        elif monto_prestamo <= 800 and periodos <= 6:
             return random.uniform(5.0, 5.2)
-        elif monto_prestamo <= 8000 and periodos <= 12:
+        elif monto_prestamo <= 800 and periodos <= 12:
             return random.uniform(5.3, 5.5)
         # Monto de 10,000
-        elif monto_prestamo <= 10000 and periodos <= 6:
+        elif monto_prestamo <= 1000 and periodos <= 6:
             return random.uniform(4.5, 4.7)
         else:
             return random.uniform(4.7, 5)
@@ -90,7 +90,7 @@ def enviar_por_correo(monto_prestamo, tasa_interes, total_intereses, total_pagos
 st.title("Simulador de Préstamo")
 
 fecha_inicio = st.date_input("Seleccione la fecha en la que desea adquirir el préstamo", value=(datetime.date.today() + datetime.timedelta(days=30)))
-monto_prestamo = st.slider("Seleccione el monto del préstamo:", min_value=0, max_value=10_000, step=100, value=1000, format="$%d")
+monto_prestamo = st.slider("Seleccione el monto del préstamo:", min_value=0, max_value=1_000, step=10, value=500, format="$%d")
 periodos = st.slider("Seleccione Duración de préstamo (en meses):", min_value=1, max_value=12, step=1, value=6, key="periodos_slider")
 primera_vez = st.radio("¿Es la primera vez que solicita un préstamo a Moonetaes?", ("Sí", "No")) == "Sí"
 
@@ -114,7 +114,7 @@ if st.button("Calcular"):
     total_intereses = sum(row[3] for row in tabla_amortizacion)
     total_pagos = sum(row[4] for row in tabla_amortizacion)
     st.info(f"**Monto de préstamo:** ${monto_prestamo:,.2f}")
-    st.info(f"**Tasa de interés asignada:** {tasa_interes:.2f}%")
+    st.info(f"**Tasa de interés mensual:** {tasa_interes:.2f}%")
     st.info(f"**Total de intereses pagados:** ${total_intereses:,.2f}")
     st.info(f"**Total de pagos realizados:** ${total_pagos:,.2f}")
 
