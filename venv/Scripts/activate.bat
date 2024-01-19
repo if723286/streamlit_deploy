4622a26238f2a7c7359cfec5a5a10897197b863f
if723286 <if723286@iteso.mx>
2024-01-19 16:02:39 -0600

    Primer commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\blas_\ITESO\streamlit_deploy\venv
+set VIRTUAL_ENV=C:\Users\LENOVO\OneDrive\GIT_TESA\TESAAPP\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
